2bd7f74ffc9029f07abbefde585ce4ecf1237f77
Sriram-098 <sriramsenapathi842@gmail.com>
2025-02-27 21:34:43 +0530

    frontend

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+const Navbar = () => {
+    const [menu,setMenu]=useState("shop");
+  return (
+    <div className="navbar">
+        <div className="nav-logo">
+            <img src={logo} alt=""/>
+            <p>NxtTrendz</p> 
+        </div>
+        <ul className="nav-menu">
+            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("women")}}>women{menu==="women"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}>kids{menu==="kids"?<hr/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button className="">Login</button>
+            <img src={cart_icon} alt="" className="" />
+            <div className="nav-cart-count">0</div>
+        </div>
+
+
+
+
+       
+    </div>
+  )
+}
+
+export default Navbar
