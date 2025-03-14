1761a91d400ec50239ee8dff8d79fd1f9cfa29d2
Sriram-098 <sriramsenapathi842@gmail.com>
2025-03-14 14:11:34 +0530

    updated shop page with footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Shopcategory from './Pages/Shopcategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           
          
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
     
