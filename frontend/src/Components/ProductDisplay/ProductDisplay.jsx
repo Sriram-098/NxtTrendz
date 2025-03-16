c34511eeb73ab3120a5ea13d2e99e037ab45dc46
Sriram-098 <sriramsenapathi842@gmail.com>
2025-03-16 22:11:36 +0530

    added product and signup pages

---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+
+const ProductDisplay = (props) => {
+    const { product } = props;
+
+    return (
+        <div className="productdisplay">
+            <div className="productdisplay-left">
+                <div className="productdisplay-img-list">
+                    <img src={product.image} alt=""/>
+                    <img src={product.image} alt=""/>
+                    <img src={product.image} alt=""/>
+                    <img src={product.image} alt=""/>
+
+                </div>
+                <div className="productdisplay-img">
+                    <img className="productdisplay-main-img" src={product.image || ''} alt="" />
+                </div>
+            </div>
+            <div className="productdisplay-right">
+                <h1>{product.name || 'Product Name'}</h1>
+                <div className="productdisplay-right-star">
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_dull_icon} alt="" />
+                    <p>{122}</p>
+                </div>
+                <div className="prouctdisplay-right-prices">
+                    <div className="productdisplay-right-price-old">${product.old_price || 0}</div>
+                    <div className="productdisplay-right-price-new">${product.new_price || 0}</div>
+                </div>
+                <div className="productdisplay-right-description">
+                    Classic Cotton Casual Shirt, designed for both comfort and style. Made from 100% premium cotton, this lightweight and breathable shirt is perfect for everyday wear. Whether you’re dressing up for a formal event or keeping it casual for a day out, this shirt offers versatility and timeless elegance.
+                </div>
+                <div className="productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className="productdisplay-right-size-options">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXL</div>
+                    </div>
+                </div>
+
+                <button>Add to Cart</button>
+                <p className="productdisplay-right-category"><span>Category:</span> Women, T-Shirt, Crop Top</p>
+                <p className="productdisplay-right-category"><span>Tags:</span> Modern, Latest</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProductDisplay;
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./Css/LoginSignup.css"
 
 const LoginSignup = () => {
   return (
-    <div>
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>sign up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="your name"/>
+          <input type="email"placeholder="email address"/>
+          <input type="password" placeholder="password"/> 
+
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account?<span>Login</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id=""/>
+          <p>By continuing ,i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
       
     </div>
   )
